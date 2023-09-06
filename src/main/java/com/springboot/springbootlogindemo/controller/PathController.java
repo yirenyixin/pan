a5003ed91f9b1a207c18e58a7a0cd12c1affe127
1 <1595668021@qq.com>
2023-09-06 23:11:55 +0800

    0906

--- src/main/java/com/springboot/springbootlogindemo/controller/PathController.java
+++ src/main/java/com/springboot/springbootlogindemo/controller/PathController.java
@@ -45,13 +45,11 @@ public class PathController {
                     Map<String, Object> rootInfo = new HashMap<>();
                     rootInfo.put("save_path", subDir.getName() + "/");
                     rootInfo.put("space", 0);
-                    rootInfo.put("children", new ArrayList<>()); // Initialize empty children list
+                    rootInfo.put("children", new ArrayList<>()); // 初始化空的子目录列表
                     rootInfo.put("uname", uname);
-
                     printDirectoryTree(subDir, rootInfo, 1);
-
                     response = rootInfo;
-                    break; // Only process the first matching subdirectory
+                    break; // 只处理第一个匹配的子目录
                 }
             }
         }
@@ -60,10 +58,7 @@ public class PathController {
         return ResponseEntity.ok(response);
     }
 
-
-
-
-
+    // 递归打印目录树
     private void printDirectoryTree(File directory, Map<String, Object> parentInfo, int depth) {
         File[] fileList = directory.listFiles();
         if (fileList != null) {
@@ -72,23 +67,25 @@ public class PathController {
                 Map<String, Object> fileInfo = new HashMap<>();
                 String savePath = file.isDirectory() ? file.getName() + "/" : file.getName();
                 fileInfo.put("save_path", savePath);
-                fileInfo.put("space", depth * 2 - 1); // Customize the space value as needed
+                fileInfo.put("space", depth * 2 - 1); // 根据需要自定义缩进空格数量
 
                 if (!file.isDirectory()) {
                     fileInfo.put("file_type", getFileType(file.getName()));
                 }
 
+                // 在这里将子节点添加到父节点的children列表中
                 children.add(fileInfo);
 
                 if (file.isDirectory()) {
-                    fileInfo.put("children", new ArrayList<>()); // Initialize empty children list
+                    fileInfo.put("children", new ArrayList<>()); // 初始化空的子目录列表
                     printDirectoryTree(file, fileInfo, depth + 1);
                 }
             }
-            parentInfo.put("children", children);
+            parentInfo.put("children", children); // 将children列表添加到父节点
         }
     }
 
+    // 获取文件类型
     private String getFileType(String fileName) {
         int dotIndex = fileName.lastIndexOf('.');
         if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
@@ -96,18 +93,16 @@ public class PathController {
         }
         return "";
     }
+
     @PostMapping("/getfile")
     public ResponseEntity<Map<String, Object>> getFileController(@RequestBody Map<String, String> requestBody) {
-
         String uname = requestBody.get("uname"); // 获取前端传递的uname值
-
         String rootPath = pathService.findAll().get(0).getSave_path(); // 根据你的需求设置根路径
         File rootDirectory = new File(rootPath);
         System.out.println(uname);
         System.out.println(rootPath);
         File[] subDirectories = rootDirectory.listFiles(File::isDirectory);
         Map<String, Object> response = new HashMap<>();
-
         if (subDirectories != null) {
             for (File subDir : subDirectories) {
                 if (subDir.getName().equals(uname)) {
@@ -124,7 +119,6 @@ public class PathController {
                 }
             }
         }
-
         System.out.println(response);
         return ResponseEntity.ok(response);
     }
@@ -138,13 +132,10 @@ public class PathController {
                     String savePath =  file.getName() + "/" ;
                     fileInfo.put("save_path", savePath);
                     fileInfo.put("space", depth * 2 - 1); // Customize the space value as needed
-
                     if (!file.isDirectory()) {
                         fileInfo.put("file_type", getFileType(file.getName()));
                     }
-
                     children.add(fileInfo);
-
                     if (file.isDirectory()) {
                         fileInfo.put("children", new ArrayList<>()); // Initialize empty children list
                         printFileDirectoryTree(file, fileInfo, depth + 1);
@@ -182,7 +173,6 @@ public class PathController {
                     return Result.error("路径不存在", "指定的路径不存在：" + path);
                 }
             }
-
             return Result.success("删除成功", "文件或文件夹删除成功！");
         } catch (IOException e) {
             e.printStackTrace();
@@ -221,7 +211,6 @@ public class PathController {
                 downloadUrls.add(downloadUrl);
             }
         }
-
         Map<String, List<String>> responseData = new HashMap<>();
         responseData.put("downloadUrls", downloadUrls);
 
@@ -258,25 +247,26 @@ public class PathController {
                                       @RequestParam("totalChunks") int totalChunks,
                                       @RequestParam("fileName") String fileName) {
         try {
-            String getPath=pathService.findAll().get(0).getSave_path()+"/";
-            String formattedPath = getPath+savePath;
+            // 获取文件保存的基础路径
+            String basePath = pathService.findAll().get(0).getSave_path() + "/";
+            // 格式化保存路径，将斜杠替换为反斜杠
+            String formattedPath = basePath + savePath;
             formattedPath = formattedPath.replace("/", "\\");
-
+            // 构建当前切片的文件名
             String chunkFileName = fileName + ".part" + chunkIndex;
+            // 构建目标文件的完整路径
             String targetFilePath = Paths.get(formattedPath, chunkFileName).toString();
-
+            // 创建目标文件
             File targetFile = new File(targetFilePath);
+            // 将切片文件内容写入目标文件
             chunk.transferTo(targetFile);
-
             if (chunkIndex == totalChunks - 1) {
-                // All chunks uploaded, combine them into the final file
+                // 所有切片上传完成，合并它们成为最终文件
                 File finalFile = new File(Paths.get(formattedPath, fileName).toString());
                 FileOutputStream outputStream = new FileOutputStream(finalFile, true);
-
                 for (int i = 0; i < totalChunks; i++) {
                     String partFileName = fileName + ".part" + i;
                     File partFile = new File(Paths.get(formattedPath, partFileName).toString());
-
                     FileInputStream inputStream = new FileInputStream(partFile);
                     byte[] buffer = new byte[1024];
                     int bytesRead;
@@ -284,59 +274,59 @@ public class PathController {
                         outputStream.write(buffer, 0, bytesRead);
                     }
                     inputStream.close();
-
-                    partFile.delete(); // Delete the chunk file
+                    partFile.delete(); // 删除切片文件
                 }
-
                 outputStream.close();
-
+                // 返回成功消息
                 return Result.success("成功", "文件上传成功");
             }
-
+            // 返回切片上传成功消息
             return Result.success("成功", "切片上传成功");
         } catch (IOException e) {
             e.printStackTrace();
+            // 返回失败消息
             return Result.error("失败", "文件切片上传失败：" + e.getMessage());
         }
     }
-    @GetMapping("/download/{filePath}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String filePath, @RequestHeader(value = "Range", required = false) String rangeHeader) throws IOException {
-        String fullPath = pathService.findAll().get(0).getSave_path() + "/" + filePath;
-        File file = new File(fullPath);
 
-        if (!file.exists() || !file.isFile()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        long fileLength = file.length();
-
-        List<HttpRange> ranges = HttpRange.parseRanges(rangeHeader);
-
-        if (ranges.isEmpty() || ranges.size() > 1) {
-            // Full file download
-            return ResponseEntity
-                    .status(HttpStatus.PARTIAL_CONTENT)
-                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                    .contentLength(fileLength)
-                    .body((Resource) new FileSystemResource(file));
-        }
-
-        HttpRange range = ranges.get(0);
-        long start = range.getRangeStart(fileLength);
-        long end = range.getRangeEnd(fileLength);
-
-        long contentLength = end - start + 1;
-
-        Path path = Paths.get(fullPath);
-        Resource resource = (Resource) new InputStreamResource(Files.newInputStream(path));
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Accept-Ranges", "bytes");
-        headers.add("Content-Range", "bytes " + start + "-" + end + "/" + fileLength);
-        headers.setContentLength(contentLength);
-
-        return new ResponseEntity<>(resource, headers, HttpStatus.PARTIAL_CONTENT);
-    }
+//    @GetMapping("/download/{filePath}")
+//    public ResponseEntity<Resource> downloadFile(@PathVariable String filePath, @RequestHeader(value = "Range", required = false) String rangeHeader) throws IOException {
+//        String fullPath = pathService.findAll().get(0).getSave_path() + "/" + filePath;
+//        File file = new File(fullPath);
+//
+//        if (!file.exists() || !file.isFile()) {
+//            return ResponseEntity.notFound().build();
+//        }
+//
+//        long fileLength = file.length();
+//
+//        List<HttpRange> ranges = HttpRange.parseRanges(rangeHeader);
+//
+//        if (ranges.isEmpty() || ranges.size() > 1) {
+//            // Full file download
+//            return ResponseEntity
+//                    .status(HttpStatus.PARTIAL_CONTENT)
+//                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+//                    .contentLength(fileLength)
+//                    .body((Resource) new FileSystemResource(file));
+//        }
+//
+//        HttpRange range = ranges.get(0);
+//        long start = range.getRangeStart(fileLength);
+//        long end = range.getRangeEnd(fileLength);
+//
+//        long contentLength = end - start + 1;
+//
+//        Path path = Paths.get(fullPath);
+//        Resource resource = (Resource) new InputStreamResource(Files.newInputStream(path));
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("Accept-Ranges", "bytes");
+//        headers.add("Content-Range", "bytes " + start + "-" + end + "/" + fileLength);
+//        headers.setContentLength(contentLength);
+//
+//        return new ResponseEntity<>(resource, headers, HttpStatus.PARTIAL_CONTENT);
+//    }
     @PostMapping("preView")
     public ResponseEntity<Map<String, List<String>>> preViewFiles(@RequestBody List<String> paths) throws IOException {
         List<String> formattedPaths = new ArrayList<>();
